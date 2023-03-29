a16daa498eba654860db8c811e8ae6c327ad6acb
BeeEye Dmu <bikang@konyang.ac.kr>
2023-03-29 10:58:41 +0900

    Add files via upload

---
+++ 020_FormClock/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace _020_FormClock
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// 필수 디자이너 변수입니다.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// 사용 중인 모든 리소스를 정리합니다.
+    /// </summary>
+    /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form 디자이너에서 생성한 코드
+
+    /// <summary>
+    /// 디자이너 지원에 필요한 메서드입니다. 
+    /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.아날로그ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.디지털ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.끝내기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.menuStrip1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.아날로그ToolStripMenuItem,
+            this.디지털ToolStripMenuItem,
+            this.끝내기ToolStripMenuItem});
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Size = new System.Drawing.Size(307, 24);
+      this.menuStrip1.TabIndex = 0;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // 아날로그ToolStripMenuItem
+      // 
+      this.아날로그ToolStripMenuItem.Name = "아날로그ToolStripMenuItem";
+      this.아날로그ToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+      this.아날로그ToolStripMenuItem.Text = "아날로그";
+      // 
+      // 디지털ToolStripMenuItem
+      // 
+      this.디지털ToolStripMenuItem.Name = "디지털ToolStripMenuItem";
+      this.디지털ToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+      this.디지털ToolStripMenuItem.Text = "디지털";
+      // 
+      // 끝내기ToolStripMenuItem
+      // 
+      this.끝내기ToolStripMenuItem.Name = "끝내기ToolStripMenuItem";
+      this.끝내기ToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+      this.끝내기ToolStripMenuItem.Text = "끝내기";
+      // 
+      // panel1
+      // 
+      this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.panel1.Location = new System.Drawing.Point(0, 24);
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size(307, 344);
+      this.panel1.TabIndex = 1;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(307, 368);
+      this.Controls.Add(this.panel1);
+      this.Controls.Add(this.menuStrip1);
+      this.MainMenuStrip = this.menuStrip1;
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem 아날로그ToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem 디지털ToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem 끝내기ToolStripMenuItem;
+    private System.Windows.Forms.Panel panel1;
+  }
+}
+
---
+++ 020_FormClock/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _020_FormClock
+{
+  public partial class Form1 : Form
+  {
+    // 이 클래스의 변수(필드)
+    private Graphics g;
+    private bool aClockFlag = true;
+    private Point center; // 중심점
+    private double radius;  // 반지름
+    private int hourHand;   // 시침의 길이
+    private int minHand;    // 분침의 길이
+    private int secHand;    // 초침의 길이
+
+    const int clientSize = 300; // 상수로 클리이언트 크기를 지정
+    const int clockSize = 200;  // 시계의 크기
+
+    public Form1()
+    {
+      InitializeComponent();
+      this.Text = "Form Clock";
+      panel1.BackColor = Color.WhiteSmoke;
+
+      this.ClientSize = new Size(clientSize, 
+        clientSize+menuStrip1.Height);
+
+      // 그래픽스 객체를 만든다
+      g = panel1.CreateGraphics();
+
+      aClockSetting();
+      TimerSetting();
+    }
+
+    private void aClockSetting()
+    {
+      center = new Point(clientSize/2, clientSize/2);
+      radius = clockSize/2;
+      hourHand = (int)(radius*0.45);
+      minHand = (int)(radius * 0.55);
+      secHand = (int)(radius * 0.65);
+    }
+
+    private void TimerSetting()
+    {
+      Timer timer = new Timer();
+      timer.Interval = 1000;
+      timer.Tick += Timer_Tick;
+      timer.Start();
+    }
+
+    // 현재 시간을 읽어와서, 시계를 그린다
+    private void Timer_Tick(object sender, EventArgs e)
+    {
+      DateTime c = DateTime.Now;
+
+      panel1.Refresh();
+
+      if(aClockFlag == true) // 아날로그
+      {
+        DrawClockFace();  // 시계판
+
+        // 시계바늘을 그린다.(핵심, 각도를 계산)
+        double radHr = (c.Hour % 12 + c.Minute / 60) * 30
+          * Math.PI / 180; // 시간당 30도
+        double radMin = (c.Minute + c.Second / 60) * 6
+          * Math.PI / 180; // 분당 6도
+        double radSec = c.Second * 6 * Math.PI / 180; //초당 6도
+
+        DrawHands(radHr, radMin, radSec);
+      }
+      else // 디지털시계
+      {
+
+      }
+    }
+
+    // 각도를 받아서 시계바늘 그리는 메소드
+    private void DrawHands(double radHr, double radMin, double radSec)
+    {
+      DrawLine(0, 0, (int)(hourHand*Math.Sin(radHr)),
+        (int)(hourHand *Math.Cos(radHr)), Brushes.RoyalBlue, 8);
+      DrawLine(0,0, (int)(minHand * Math.Sin(radMin)),
+        (int)(minHand * Math.Cos(radMin)), Brushes.SkyBlue, 6);
+      DrawLine(0, 0, (int)(secHand * Math.Sin(radSec)),
+        (int)(secHand * Math.Cos(radSec)), Brushes.OrangeRed, 4);
+
+      // 배꼽
+      int coreSize = 16;
+      Rectangle r = new Rectangle(
+        center.X - coreSize/2, center.Y - coreSize/2,
+        coreSize, coreSize);
+      g.FillEllipse(Brushes.Gold, r);
+      g.DrawEllipse(new Pen(Brushes.Green, 3), r);
+    }
+
+    private void DrawLine(int x1, int y1, int x2, int y2, Brush brush, int thick)
+    {
+      Pen p = new Pen(brush, thick);
+      p.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+      g.DrawLine(p, center.X + x1, center.Y + y1, 
+        center.X + x2, center.Y - y2);
+    }
+
+    private void DrawClockFace()
+    {
+      Pen p = new Pen(Color.LightSteelBlue, 30);
+      g.DrawEllipse(p, center.X-clockSize/2,
+        center.Y-clockSize/2, clockSize, clockSize);
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
